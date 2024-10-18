5179e3bb24f8256ff6718070f44868b0e1c3bca4
asif-kamal <asif.kamal1489@gmail.com>
2024-10-18 13:25:47 -0400

    Updated App component using JSX, nesting NavBar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './layout/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
