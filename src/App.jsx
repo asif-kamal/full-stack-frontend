b4c8c90e909b7753993b80b0d8ae813b76e392da
asif-kamal <asif.kamal1489@gmail.com>
2024-10-18 20:57:14 -0400

    created Home component rendering a table with users from DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './layout/NavBar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
