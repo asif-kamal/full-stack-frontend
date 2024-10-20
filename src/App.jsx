34563aea4dcf36d53043877db62012e30b25acb4
asif-kamal <asif.kamal1489@gmail.com>
2024-10-19 20:08:22 -0400

    Utilized BrowserRouter to direct traffic to home, register endpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import NavBar from './layout/NavBar';
-import Home from './pages/Home';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./layout/NavBar";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterUser from "./users/RegisterUser";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Home />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/register" element={<RegisterUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
