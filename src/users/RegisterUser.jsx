c3ad16111fae68b1d55978e04318875b3400b1db
asif-kamal <asif.kamal1489@gmail.com>
2024-10-21 13:44:40 -0400

    added form tag and created onSubmit func to add new user to DB

--- src/users/RegisterUser.jsx
+++ src/users/RegisterUser.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function RegisterUser() {
+  let navigate = useNavigate();
   const [user, setUser] = useState({
     name: "",
     username: "",
@@ -12,56 +15,61 @@ export default function RegisterUser() {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/users/register", user);
+    navigate("/");
+  };
   return (
     <div className="register-user-container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Register User</h2>
-          <div className="mb-3">
-            <label htmlFor="Name" className="form-label">
-              Full Name
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter your full name"
-              name="name"
-              value={name}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="Username" className="form-label">
-              Username
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter your username"
-              name="username"
-              value={username}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="Email" className="form-label">
-              Email
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter your email address"
-              name="email"
-              value={email}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <button type="submit" className="btn btn-outline-primary">
-            Submit
-          </button>
-          <button type="submit" className="btn btn-outline-danger mx-2">
-            Cancel
-          </button>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Full Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your full name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Username" className="form-label">
+                Username
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your username"
+                name="username"
+                value={username}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                Email
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your email address"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <Link className="btn btn-outline-danger mx-2" to="/users">Cancel</Link>
+          </form>
         </div>
       </div>
     </div>
