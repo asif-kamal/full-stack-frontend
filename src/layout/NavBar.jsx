359b7ec24f1d7cd950f856e40f0b1dd49338097f
asif-kamal <asif.kamal1489@gmail.com>
2024-10-24 20:37:29 -0400

    added Link to Return to Home Button

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -6,9 +6,9 @@ export default function NavBar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" to={"/users"}>
             Full-Stack Application
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
